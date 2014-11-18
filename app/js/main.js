f8e2db79a082ae1f272c17e8eec561cb1a3a0d8a
Steven Stevenson <stevensonhoyt@icloud.com>
2014-11-18 13:53:14 -0800

    fixed more copy and links

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
+var popupwindow = function(url,name,height,width) {
+	newwindow=window.open(url,name,'height='+height+',width='+width);
+	if (window.focus) {newwindow.focus()}
+	return false;
+}
+
 $(document).ready( function(){
+	var teaserOpened = false;
 
 	$('#more-about-edith').on('click',function(){
-		$('.header-info').slideDown();
+		if (teaserOpened) {
+			$('.header-info').slideUp();
+			teaserOpened = false;
+		} else {
+			$('.header-info').slideDown();
+			teaserOpened = true;
+		}
 	});
 
 	$('#less-about-edith').on('click', function() {
@@ -14,6 +27,13 @@ $(document).ready( function(){
 		$('.main-content').animate({
 			marginTop: 0
 		}, 200)
+	});
+
+	$(window).resize( function() {
+		if (teaserOpened) {
+			teaserOpened = false;
+			$('.header-info').slideUp();
+		}
 	});
 
 	console.log('main loaded');
